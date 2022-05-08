d540e32ebc73d2e94319edbba6c4b8bd61afe12f
Cretu Andra <Cretu.Mi.Andra@student.utcluj.ro>
2022-05-08 16:16:02 +0300

    ceva

--- c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
@@ -13,7 +13,7 @@ public class Shop {
 
     public Shop(String name) {
         this.city = "";
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Andra");
     }
 
     public String getName() {
