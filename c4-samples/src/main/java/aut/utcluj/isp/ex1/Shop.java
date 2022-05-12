259aa837e1401972b990bdfd8f98a33f28b778f8
hepes-bogdan-30127 <bogdan.hepes@gmail.com>
2022-05-12 18:49:46 +0300

    ex1 si ex2

--- c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
@@ -8,21 +8,13 @@ public class Shop {
     private String city;
 
     public Shop(String name, String city) {
-<<<<<<< HEAD
         this.city=city;
         this.name=name;
-=======
-        throw new UnsupportedOperationException("Not supported yet.");
->>>>>>> ca7faa0b392b393f6f830a39c61f0467dd21da4a
     }
 
     public Shop(String name) {
         this.city = "";
-<<<<<<< HEAD
         this.name=name;
-=======
-        throw new UnsupportedOperationException("Not supported yet.");
->>>>>>> ca7faa0b392b393f6f830a39c61f0467dd21da4a
     }
 
     public String getName() {
